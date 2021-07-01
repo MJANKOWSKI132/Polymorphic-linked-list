65446009e6a8a0741815cf27cc332019dc19c316
Michael Jankowski <mjankowski@student.unimelb.edu.au>
2021-07-01 18:40:21 +1000

    Started writing tests

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
 // This linked list can store the following primitive data types
 enum Type {INTEGER = 0, STRING = 1, FLOAT = 2, CHAR = 3};
@@ -15,43 +16,55 @@ struct node{
 
 // Function prototypes
 void printLinkedList(struct node* head);
-void freeLinkedList(struct node* head, void (*freeFunction)(void*));
+void clear(struct node** head, void (*freeFunction)(void*));
+void clearTest();
 void addToEnd(struct node** head, struct node* insertNode);
+void addToEndTest();
 void addToStart(struct node** head, struct node* insertNode);
+void addToStartTest();
 struct node* createNode(void* value, enum Type type);
+void createNodeTest();
 void printNode(struct node* node);
 int getLength(struct node* head);
+void getLengthTest();
 struct node* getHead(struct node** head, bool remove);
+void getHeadTest();
 struct node* getFoot(struct node** head, bool remove);
+void getFootTest();
 struct node* get(struct node** head, int index, bool remove);
+void getTest();
 bool isEmpty(struct node* head);
+void isEmptyTest();
 bool isHomogeneous(struct node* head);
-struct node* findFirstIter(struct node* head, bool (*cmp)(void*));
-struct node* findFirstRec(struct node* head, bool (*cmp)(void*));
-struct node* findAllIter(struct node* head, bool (*cmp)(void*));
-struct node* findAllRecUtility(struct node* head, bool (*cmp)(void*), struct node* newHead);
-struct node* findAllRec(struct node* head, bool(*cmp)(void*));
+void isHomogeneousTest();
+struct node* findFirstIter(struct node* head, bool (*predicate)(void*));
+void findFirstIterTest();
+struct node* findFirstRec(struct node* head, bool (*predicate)(void*));
+void findFirstRecTest();
+struct node* findAllIter(struct node* head, bool (*predicate)(void*));
+void findAllIterTest();
+struct node* findAllRecUtility(struct node* head, bool (*predicate)(void*), struct node* newHead);
+struct node* findAllRec(struct node* head, bool(*predicate)(void*));
+void findAllRecTest();
 bool isEqual(struct node* firstNode, struct node* secondNode);
+void isEqualTest();
 void reverse(struct node** head);
+void reverseTest();
 struct node* map(struct node* head, void* (*f)(void*));
+void mapTest();
 void forEach(struct node* head, void (*f)(void*));
+void forEachTest();
 struct node* filter(struct node* head, bool (*predicate)(void*));
+void filterTest();
 
 bool isInteger(struct node* node){
     return node -> type == INTEGER;
 }
 
 void main(){
-    struct node* head = (struct node*)malloc(sizeof(struct node));
-    head = NULL;
-    int numbers[5] = {1, 2, 3, 4, 5};
-    for(int i = 0; i < 5; i ++){
-        struct node* insertNode = createNode(&numbers[i], INTEGER);
-        addToEnd(&head, insertNode);
-    }
-    struct node* filteredHead = filter(head, isInteger);
-    forEach(filteredHead, printNode);
-    freeLinkedList(head, free);
+    clearTest();
+    createNodeTest();
+    addToEndTest();
 }
 
 // A function to print each node
@@ -86,8 +99,8 @@ void printLinkedList(struct node* head){
 }
 
 // A function to free the entire linked list
-void freeLinkedList(struct node* head, void (*freeFunction)(void*)){
-    struct node* currNode = head;
+void clear(struct node** head, void (*freeFunction)(void*)){
+    struct node* currNode = *head;
     struct node* prevNode = NULL;
     // Iterate over each node freeing it, ensuring that you store the next node before freeing the current
     while(currNode){
@@ -95,7 +108,23 @@ void freeLinkedList(struct node* head, void (*freeFunction)(void*)){
         currNode = currNode -> next;
         freeFunction(prevNode);
     }
+    *head = NULL;
 }
+
+void clearTest(){
+    struct node* head = (struct node*)malloc(sizeof(struct node));
+    head = NULL;
+    int NUMBERS_LENGTH = 5;
+    int numbers[5] = {1, 2, 3, 4, 5};
+    struct node** nodes = (struct node**)malloc(NUMBERS_LENGTH * sizeof(struct node*));
+    for(int i = 0; i < NUMBERS_LENGTH; i ++){
+        struct node* insertNode = createNode(&numbers[i], INTEGER);
+        addToEnd(&head, insertNode);
+    }
+    clear(&head, free);
+    assert(head == NULL);
+}
+
 
 // A function to create a node based on a value and a specified type
 struct node* createNode(void* value, enum Type type){
@@ -122,6 +151,19 @@ struct node* createNode(void* value, enum Type type){
     return node;
 }
 
+void createNodeTest(){
+    int value = 10;
+    enum Type type = INTEGER;
+    struct node* node = createNode(&value, type);
+    assert(node != NULL);
+    int nodeValue;
+    memcpy(&nodeValue, &(node -> value), sizeof(int));
+    assert(nodeValue == 10);
+    assert(node -> type == INTEGER);
+    assert(node -> next == NULL);
+    free(node);
+}
+
 // A function to add to the end (foot) of the linked list
 void addToEnd(struct node** head, struct node* insertNode){
     if(!insertNode) return;
@@ -136,6 +178,28 @@ void addToEnd(struct node** head, struct node* insertNode){
         }
         currNode -> next = insertNode;
     }
+}
+
+void addToEndTest(){
+    struct node* head = (struct node*)malloc(sizeof(struct node));
+    head = NULL;
+    int LENGTH = 5;
+    int integers[5] = {1, 2, 3, 4, 5};
+    char characters[5] = {'a', 'b', 'c', 'd', 'e'};
+    char* strings[5] = {"string1", "string2", "string3", "string4", "string5"};
+    float floats[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    for(int i = 0; i < LENGTH; i ++){
+        struct node* insertNode1 = createNode(&integers[i], INTEGER);
+        struct node* insertNode2 = createNode(&characters[i], CHAR);
+        struct node* insertNode3 = createNode(&strings[i], STRING);
+        struct node* insertNode4 = createNode(&floats[i], FLOAT);
+        addToEnd(&head, insertNode1);
+        addToEnd(&head, insertNode2);
+        addToEnd(&head, insertNode3);
+        addToEnd(&head, insertNode4);
+    }
+    printLinkedList(head);
+    clear(&head, free);
 }
 
 // A function to add to the start (head) of the linked list
@@ -278,10 +342,12 @@ bool isEqual(struct node* firstNode, struct node* secondNode){
     return false;
 }
 
-struct node* findFirstIter(struct node* head, bool (*cmp)(void*)){
+// A function that finds the first node that satisfies the given predicate
+// Utilises an iterative implementation of linear search
+struct node* findFirstIter(struct node* head, bool (*predicate)(void*)){
     struct node* currNode = head;
     while(currNode){
-        if(cmp(currNode)){
+        if(predicate(currNode)){
             return currNode;
         }
         currNode = currNode -> next;
@@ -289,37 +355,46 @@ struct node* findFirstIter(struct node* head, bool (*cmp)(void*)){
     return NULL;
 }
 
-struct node* findFirstRec(struct node* head, bool (*cmp)(void*)){
+// A function that finds the first node that satisfies the given predicate
+// Utilises a recursive implementation of linear search
+struct node* findFirstRec(struct node* head, bool (*predicate)(void*)){
     if(head == NULL) return NULL;
-    if(cmp(head)) return head;
-    return findFirstRec(head -> next, cmp);
+    if(predicate(head)) return head;
+    return findFirstRec(head -> next, predicate);
 }
 
-struct node* findAllIter(struct node* head, bool (*cmp)(void*)){
+// A function that finds all nodes that satisfy the given predicate and returns such nodes as a new linked list
+// Utilises an iterative implementation of linear search
+struct node* findAllIter(struct node* head, bool (*predicate)(void*)){
     struct node* currNode = head;
     struct node* newHead = (struct node*)malloc(sizeof(struct node));
     newHead = NULL;
     while(currNode){
-        if(cmp(currNode)){
-            addToEnd(&newHead, currNode);
+        if(predicate(currNode)){
+            struct node* insertNode = createNode(&(currNode -> value), currNode -> type);
+            addToEnd(&newHead, insertNode);
         }
         currNode = currNode -> next;
     }
     return newHead;
 }
 
-struct node* findAllRecUtility(struct node* head, bool (*cmp)(void*), struct node* newHead){
+// Recursive utility function for findAllRec
+struct node* findAllRecUtility(struct node* head, bool (*predicate)(void*), struct node* newHead){
     if(head == NULL) return newHead;
-    if(cmp(head)){
-        addToEnd(&newHead, head);
+    if(predicate(head)){
+        struct node* insertNode = createNode(&(head -> value), head -> type);
+        addToEnd(&newHead, insertNode);
     }
-    return findAllRecUtility(head -> next, cmp, newHead);
+    return findAllRecUtility(head -> next, predicate, newHead);
 }
 
-struct node* findAllRec(struct node* head, bool (*cmp)(void*)){
+// A function that finds all nodes that satisfy the given predicate and returns such nodes as a new linked list
+// Utilises a recursive implementation of linear search
+struct node* findAllRec(struct node* head, bool (*predicate)(void*)){
     struct node* newHead = (struct node*)malloc(sizeof(struct node));
     newHead = NULL;
-    return findAllRecUtility(head, cmp, newHead);
+    return findAllRecUtility(head, predicate, newHead);
 }
 
 // A function to reverse a linked list
@@ -362,9 +437,8 @@ struct node* filter(struct node* head, bool (*predicate)(void*)){
     newHead = NULL;
     while(currNode){
         if(predicate(currNode)){
-            printf("here 1");
-            addToEnd(&newHead, currNode);
-            printf("here 2");
+            struct node* insertNode = createNode(&(currNode -> value), currNode -> type);
+            addToEnd(&newHead, insertNode);
         }
         currNode = currNode -> next;
     }
